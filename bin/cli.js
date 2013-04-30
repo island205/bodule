7de330c5e523eb075aea628fc70c73f194770840
island205 <island205@gmail.com>
2013-04-30 13:19:40 +0800

    add watch

--- bin/cli.js
+++ bin/cli.js
@@ -3,6 +3,7 @@
 var program = require('commander'),
     bodule = require('../').bodule,
     fs = require('fs'),
+    watch = require('watch'),
     path = require('path')
 
 program
@@ -15,27 +16,48 @@ var src = program.args[0] || '.',
     dest = program.output || '.bodule',
     type = program.type || 'amd'
 
-if (!fs.existsSync(dest)) {
-    fs.mkdirSync(dest)
-}
+// Comile file in `filePath` to `dest`
 
-var file, fileName, files
-if (src.indexOf('.js') > -1) {
+function wrapFile(filePath, type, dest) {
+    var file, fileName
     fileName = path.basename(src)
-    file = fs.readFileSync(src, 'utf8')
+    file = fs.readFileSync(filePath, 'utf8')
     file = bodule(file, {
         type: type
     })
     fs.writeFileSync(dest + '/' +  fileName, file)
-} else {
-    files = fs.readdirSync(src)
+    console.log(filePath + ' is wraped to ' + dest)
+}
+
+function wrapDir(dirPath, type, dest) {
+    var files = fs.readdirSync(src)
     files.forEach(function (file) {
-        fileName = path.basename(file)
-        file = fs.readFileSync(src + file, 'utf8')
-        file = bodule(file, {
-            type: type
-        })
-        fs.writeFileSync(dest + '/' +  fileName, file)
-            
+        wrapFile(src + file, type, dest)
     }) 
+}
+
+if (!fs.existsSync(dest)) {
+    fs.mkdirSync(dest)
+}
+
+if (program.watch) {
+    if (fs.statSync(src).isDirectory()) {
+        console.log(src + ' is watched!')
+        watch.createMonitor(src, function (monitor) {
+            monitor.on("created", function (f, stat) {
+                wrapFile(f, type, dest)
+            })
+            monitor.on("changed", function (f, curr, prev) {
+                wrapFile(f, type, dest)
+            })
+        })
+    }
+}
+
+
+var files = []
+if (src.indexOf('.js') > -1) {
+    wrapFile(src, type, dest)
+} else {
+    wrapDir(src, type, dest)
 } 
